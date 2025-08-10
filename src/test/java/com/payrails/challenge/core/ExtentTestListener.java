e1cf0a10cfcad458740ac389c42bb9769afe213d
Moayed Badawy <badawymoayed@gmail.com>
2025-08-10 08:46:04 +0300

    feat: utilizing extentReports and make downloadable from the artifacts

---
+++ src/test/java/com/payrails/challenge/core/ExtentTestListener.java
@@ -0,0 +1,44 @@
+package com.payrails.challenge.core;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtentTestListener implements ITestListener {
+
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        String reportPath = "target/extent-report.html";
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extent.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.get().log(Status.PASS, "Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.get().log(Status.FAIL, "Test failed: " + result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
